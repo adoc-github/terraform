02505f0f32d1f2b96608d39818fe7f2117ba696c
adoc-github <65012009+adoc-github@users.noreply.github.com>
2020-06-23 10:44:10 +0900

    Change instance_class.

--- rds-mysql/main.tf
+++ rds-mysql/main.tf
@@ -29,7 +29,7 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.medium"
+  instance_class       = "db.t2.micro"
   identifier           = "rds-${var.sandbox_id}"
   name                 = "${var.db_name}"
   username             = "${var.username}"
